c2f6142c0cd8b5ae3409378e0ca5545ae222c79e
Hammad Majid <hammadmajid@proton.me>
2024-12-12 19:30:41 +0500

    feat: add new color palette for light theme in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,13 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'light-body': '#fbfefb',
+        'light-text-primary': '#282728',
+        'light-text-secondary': '#525152',
+        'light-text-tertiary': '#6f6f6f',
+        'light-accent': '#1678b3',
+        'light-accent-hover': '#0f5f8b',
+        'light-border': '#d4d4d4',
       },
     },
   },
