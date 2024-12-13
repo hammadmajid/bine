8fc5440b56f2a5ffbd153436e1ae5d0504fc4ee7
Hammad Majid <hammadmajid@proton.me>
2024-12-13 17:22:20 +0500

    chore: reformat code

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,25 +12,30 @@ export default function ContactPage() {
       <main id="main-content" className="space-y-2">
         <h1 className="text-2xl font-semibold">Contact</h1>
         <p>
-          You can send me an email at <Link
+          You can send me an email at{" "}
+          <Link
             className="text-light-accent hover:text-light-accent-hover"
             href="mailto:hammadmajid@proton.me"
           >
-            hammadmajid@proton.me</Link
-          >. Please allow a few days for a response.
+            hammadmajid@proton.me
+          </Link>
+          . Please allow a few days for a response.
         </p>
       </main>
       <section className="space-y-2">
         <h2 className="text-lg font-medium">PGP key</h2>
         <p>
-          This is optional but I recommned that you encrypt the email you
-          are sending using a PGP key. You can find my PGP key on <Link
+          This is optional but I recommned that you encrypt the email you are
+          sending using a PGP key. You can find my PGP key on{" "}
+          <Link
             href="https://keys.openpgp.org/vks/v1/by-fingerprint/A40C364D8F90407DE3F057F05C22E7B2459D1E5E"
             className="text-light-accent hover:text-light-accent-hover"
-          >keys.openpgp.org</Link
-          >.
+          >
+            keys.openpgp.org
+          </Link>
+          .
         </p>
       </section>
     </div>
-  )
+  );
 }
